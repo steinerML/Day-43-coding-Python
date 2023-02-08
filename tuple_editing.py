a2c4068ca4b95915792f0f6c45951082cbaa4959
steinerML <102678863+steinerML@users.noreply.github.com>
2023-02-08 16:46:21 +0100

    Update tuple_editing.py

--- tuple_editing.py
+++ tuple_editing.py
@@ -29,3 +29,36 @@ print(a_tuple)
 #Tuple concatenation
 a_tuple = a_tuple + (888.88,)
 print(a_tuple)
+
+#More operations
+
+tuple1 = ("apple", "banana", "banana", "banana", "kiwi","melon")
+
+#Access tuples
+print(tuple1[0])
+print(tuple1[1])
+print(tuple1[2])
+print(tuple1[-1])
+
+#Add tuple
+ # tup2[2] = 4 #Error, tuples are immutable.
+ 
+#Tuple concatenation
+tuple1 = tuple1 + (4,)
+print(tuple1)
+
+#List conversion
+list1 = list(tuple1)
+list1.append('watermelon')
+tuple1 = tuple(list1)
+print(tuple1)
+
+#Tuple unpacking
+tuple1 = (*tuple1,88)
+print(tuple1)
+
+print(tuple1.index('banana'))
+print(tuple1.count('banana'))
+
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
